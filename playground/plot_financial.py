3890ed2dfc857f45bb2e3d8171527f71eb8498be
Matthew Evans <git@ml-evs.science>
2024-05-08 17:42:16 +0100

    Make example code session verbose

--- playground/plot_financial.py
+++ playground/plot_financial.py
@@ -2,7 +2,7 @@ from codeinterpreterapi import CodeInterpreterSession, settings
 
 
 # create a session and close it automatically
-with CodeInterpreterSession() as session:
+with CodeInterpreterSession(verbose=True) as session:
     # generate a response based on user input
     response = session.generate_response(
         "Plot the bitcoin chart of year 2023"
